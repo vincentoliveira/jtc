a3e26a3084207567981e1a97e48ca2e024121564
Vincent Oliveira <vincent@oliveira.fr>
2013-11-13 02:00:54 +0100

    Add Behat tests

---
+++ src/Jtc/DefaultBundle/Features/10_default.feature
@@ -0,0 +1,5 @@
+Feature: Default feature
+
+Scenario: 10.1 - Test page
+	  Given I am on "/testme"
+	  Then I should see "It works!"
---
+++ src/Jtc/DefaultBundle/Features/Context/FeatureContext.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Jtc\DefaultBundle\Features\Context;
+
+use Symfony\Component\HttpKernel\KernelInterface;
+use Behat\Symfony2Extension\Context\KernelAwareInterface;
+use Behat\MinkExtension\Context\MinkContext;
+use Behat\Behat\Context\Step;
+
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Feature context.
+ */
+class FeatureContext extends MinkContext implements KernelAwareInterface
+{
+    private $kernel;
+    private $parameters;
+
+    /**
+     * Initializes context with parameters from behat.yml.
+     *
+     * @param array $parameters
+     */
+    public function __construct(array $parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Sets HttpKernel instance.
+     * This method will be automatically called by Symfony2Extension ContextInitializer.
+     *
+     * @param KernelInterface $kernel
+     */
+    public function setKernel(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+    
+    /**
+     * @Given /^I am logged as "([^"]*)" password "([^"]*)"$/
+     */
+    public function iAmLoggedAsPassword($user, $pwd)
+    {
+        /*
+         * And I fill in "_username" with "test"
+         */
+        $steps[] = new Step\Given('I am on "/login"');
+        $steps[] = new Step\Given('I fill in "_username" with "'.$user.'"');
+        $steps[] = new Step\Given('I fill in "_password" with "'.$pwd.'"');
+        $steps[] = new Step\Given('I press "_submit"');
+        $steps[] = new Step\Then('I should see "Logged in as"');
+        
+        return $steps;
+    }
+}
