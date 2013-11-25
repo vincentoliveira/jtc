72fb7d5023e3db5fa99197385c9f7998e1b7357c
Vincent Oliveira <vincent@oliveira.fr>
2013-11-25 19:52:33 +0100

    Datepicker format

--- web/js/main.js
+++ web/js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     var date = new Date();
     date.setDate(date.getDate()-1);
     $('input.date').datepicker({
-         minDate: date
+         minDate: date,
+         dateFormat: 'dd/mm/yy'
     });
 });
